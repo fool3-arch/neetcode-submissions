9d39eb0e8466ab019d0310e46f92f74cce041731
fool3-arch <ranjive3@gmail.com>
2026-05-19 23:55:55 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0 
+        num=0
+        for i in nums:
+            if count==0:
+                num=i
+            if num == i:
+                count +=1
+            else:
+                count-=1
+        return num
+            
